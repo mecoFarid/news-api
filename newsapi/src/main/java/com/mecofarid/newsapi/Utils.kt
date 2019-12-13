85f2e410c63868b9bd3491d838a2f85bcfca866c
Farid Mammadov <farid.mammadov8118@gmail.com>
2019-12-13 22:00:41 +0900

    Test added for Util class

--- newsapi/src/main/java/com/mecofarid/newsapi/Utils.kt
+++ newsapi/src/main/java/com/mecofarid/newsapi/Utils.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
 import java.util.*
 
-internal fun Date.toNewsApiDateFormat(): String {
+fun Date.asNewsApiDateFormat(): String {
     @SuppressLint("SimpleDateFormat")
     val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
     return sdf.format(this)
--- newsapi/src/test/java/com/mecofarid/newsapi/NewsApiTest.kt
+++ newsapi/src/test/java/com/mecofarid/newsapi/NewsApiTest.kt
@@ -1,6 +1,38 @@
 package com.mecofarid.newsapi
 
+import com.mecofarid.newsapi.model.response.SourcesResponse
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewsApiTest {
+
+    private lateinit var mMockWebServer: MockWebServer
+    private lateinit var mNewsApi: NewsApi
+
+    @Before
+    fun init(){
+        mMockWebServer = MockWebServer()
+
+        mNewsApi = NewsApi()
+    }
+
+    @After
+    fun finish(){
+        mMockWebServer.shutdown()
+    }
+
+    @Test
+    fun utils_asNewsApiDate(){
+        val date = Date()
+
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val dateAsNewsApiDate = sdf.format(date)
+        assert(dateAsNewsApiDate == date.asNewsApiDateFormat())
+    }
 }
